ebdb41aace54899b1e099e5d475cd970f74415f9
UtsavGohel <utsavgohel2002@gmail.com>
2024-12-14 18:38:52 +0530

    feat(context): implement App Context for users

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+  const [user, setUser] = useState(null);
+
+  const value = {
+    user,
+    setUser,
+  };
+
+  return (
+    // eslint-disable-next-line react/prop-types
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
