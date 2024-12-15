fe3319d6394947200fe14ed9366d1a3bde7b32e4
UtsavGohel <utsavgohel2002@gmail.com>
2024-12-15 19:12:12 +0530

    refactor(header): add motion and onclick inside header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,37 @@
+import { useContext } from "react";
 import { assets } from "../assets/assets";
-
+import { motion } from "motion/react";
+import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+  const { user, setShowLogin } = useContext(AppContext);
+
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    if (user) {
+      navigate("/result");
+    } else {
+      setShowLogin(true);
+    }
+  };
   return (
-    <div className="flex flex-col justify-center items-center text-center my-20">
-      <div className="text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500">
+    <motion.div
+      className="flex flex-col justify-center items-center text-center my-20"
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <motion.div
+        className="text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2, duration: 0.8 }}
+      >
         <p>Best text to image generator</p>
         <img src={assets.star_icon} alt="" />
-      </div>
+      </motion.div>
       <h1 className="text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center">
         Turn text to the <span className="text-blue-600">image</span> in
         seconds.
@@ -16,7 +41,10 @@ const Header = () => {
         in seconds - just type, and watch the magic happen.
       </p>
 
-      <button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full">
+      <button
+        className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full"
+        onClick={onClickHandler}
+      >
         Generate Images <img className="h-6" src={assets.star_group} alt="" />
       </button>
 
@@ -34,7 +62,7 @@ const Header = () => {
           ))}
       </div>
       <p className="mt-2 text-neutral-600"> Generated images from imaginexAI</p>
-    </div>
+    </motion.div>
   );
 };
 
